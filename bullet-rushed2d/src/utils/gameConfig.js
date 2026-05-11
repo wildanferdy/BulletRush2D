d04c334ac6a360e8a5cb20d1282d4ff4e5729615
Wildan Ferdyansyah <fors8776@gmail.com>
2026-05-11 11:41:34 +0700

    config game

---
+++ bullet-rushed2d/src/utils/gameConfig.js
@@ -0,0 +1,16 @@
+export const LEVELS = {
+  easy: { label: "Eesy", duration: 30 },
+  medium: { label: "Medium", duration: 20 },
+  hard: { label: "Hard", duration: 15 },
+};
+
+export const GUNS = [
+  { id: "gun1", label: "Pistol 1", src: "./images/gun1.png" },
+  { id: "gun2", label: "Pistol 2", src: "./images/gun2.png" },
+];
+
+export const TARGETS = [
+  { id: "target1", label: "Target 1", src: "/images/target1.png", points: 10 },
+  { id: "target2", label: "Target 2", src: "/images/target2.png", points: 20 },
+  { id: "target3", label: "Target 3", src: "/images/target3.png", points: 30 },
+];
